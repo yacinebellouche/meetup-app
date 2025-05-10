f53beaef6c74799ed02158686576c69abea28e92
Bellouche Yacine <yacine.bellouche@etudiant.univ-brest.fr>
2025-05-10 14:27:11 +0200

    Added getStaticProps for detailMeetupPage

--- pages/[meetId]/index.js
+++ pages/[meetId]/index.js
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import MeetUpDetail from "../../components/meetups/MeetUpDetail";
-export default function DetailMeetPage() {
+export default function DetailMeetPage(props) {
   const router = useRouter();
   return (
     <MeetUpDetail
@@ -10,4 +10,36 @@ export default function DetailMeetPage() {
       description="This is a first meetup"
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false, // allows to pregenarate the path if it wasn't supported in the object we sent (false means it would return a 404 not found while with true it would generate the page dynamically )
+    paths: [
+      {
+        params: {
+          meetId: "meetid1",
+        },
+      },
+      {
+        params: {
+          meetId: "meetid2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetId; // meetId because it is the ID we used for the page id [meetId]
+  return {
+    props: {
+      meetupData: {
+        img: "https://upload.wikimedia.org/wikipedia/commons/7/74/Alger-Aurore.jpg",
+        title: "First Meetup",
+        address: " Some Street",
+        description: "This is a first meetup",
+      },
+    },
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const DUMBY_DATA = [
       "https://upload.wikimedia.org/wikipedia/commons/7/74/Alger-Aurore.jpg",
     title: "This is Just a meet up",
     address: "7 rue de coulmiers",
-    description: "random text"
+    description: "random text",
   },
   {
     id: "meetid2",
@@ -16,7 +16,7 @@ const DUMBY_DATA = [
       "https://upload.wikimedia.org/wikipedia/commons/7/74/Alger-Aurore.jpg",
     title: "This is Just a meetup2",
     address: "7 rue de coulmiers",
-    description: "random text"
+    description: "random text",
   },
   {
     id: "meetid3",
@@ -24,7 +24,7 @@ const DUMBY_DATA = [
       "https://upload.wikimedia.org/wikipedia/commons/7/74/Alger-Aurore.jpg",
     title: "This is Just a meet up3",
     address: "7 rue de coulmiers",
-    description: "random text"
+    description: "random text",
   },
 ];
 
@@ -36,14 +36,25 @@ function HomePage(props) {
   );
 }
 
-export async function getStaticProps(){
-    // fetch data from API
-    return {
-        props: {
-            meetups: DUMBY_DATA
-
-        }
-    }
+export async function getStaticProps() {
+  // fetch data from API
+  return {
+    props: {
+      meetups: DUMBY_DATA,
+    },
+    revalidate: 10, //allows to re pregenerate this page (upon request for it) on the server every 10 seconds, this way we make sure that our page is up to date
+  };
 }
 
+/*export async function getServerSideProps(context) {
+  const req = context.req; // auth, session cookie, request body...etc
+  const res = context.res; // we can manipulate the response object so we can return our props object
+
+  return {
+    props: {
+      meetups: DUMBY_DATA,
+    },
+  };
+}
+*/
 export default HomePage;
